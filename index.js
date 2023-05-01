46ada918776d361f5034f75fa301db175420127b
Cody Roberts <pithlyx@gmail.com>
2023-05-01 11:37:32 -0600

    Fixed JS functions

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if (rideLength <= 400) {
+    return "This one is on me!";
+  }
+  else if (rideLength > 400 && rideLength <= 2000) {
+    return "That will be twenty bucks.";
+  }
+  else if (rideLength > 2000 && rideLength <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else
+    return "No can do.";
+
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
